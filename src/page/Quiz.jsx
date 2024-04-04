b5a05d654f27648822242cad22d475aaf2c56b59
jihye1116 <jihyesim170@gmail.com>
2024-04-04 16:03:46 -0700

    design: 퀴즈 페이지 간격 조정

--- src/page/Quiz.jsx
+++ src/page/Quiz.jsx
@@ -23,41 +23,48 @@ const Quiz = () => {
 
   return (
     <Layout>
-      <div className="box-border pt-20 px-4 min-h-screen flex flex-col items-center justify-center">
+      <div className="pt-40 px-4 min-h-screen flex flex-col items-center bg-gray-100 text-gray-800">
         {showScore ? (
           <div className="score-section text-lg font-semibold">
             당신의 점수는 <span className="text-green-500">{score}</span>점
             입니다!
+            <div className="mt-4 text-sm">
+              <button
+                onClick={() => window.location.reload()}
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline shadow"
+              >
+                다시 플레이
+              </button>
+            </div>
           </div>
         ) : (
-          <>
-            <div className="question-section mb-4">
-              <div className="question-count mb-2">
-                <span className="text-xl font-bold">
-                  문제 {currentQuestion + 1}
-                </span>
-                /{quizData.length}
-              </div>
-              <div className="question-text text-lg font-medium">
-                {quizData[currentQuestion].question}
-              </div>
+          <div className="question-section mb-8 w-96 flex flex-col gap-8">
+            <div className="question-count mb-2">
+              <span className="text-xl font-bold">
+                문항 {currentQuestion + 1}
+              </span>
+              /{quizData.length}
             </div>
-            <div className="answer-section grid grid-cols-2 gap-4">
+            <div className="question-text text-lg font-medium">
+              {quizData[currentQuestion].question}
+            </div>
+            <div className="answer-section">
               {quizData[currentQuestion].options.map((option, index) => (
-                <button
-                  key={index}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  onClick={() =>
-                    handleAnswerOptionClick(
-                      option === quizData[currentQuestion].answer
-                    )
-                  }
-                >
-                  {option}
-                </button>
+                <div key={index} className="flex items-center">
+                  <input
+                    type="checkbox"
+                    className="appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-500 checked:border-transparent focus:outline-none"
+                    onChange={() =>
+                      handleAnswerOptionClick(
+                        option === quizData[currentQuestion].answer
+                      )
+                    }
+                  />
+                  <label className="ml-2 text-md font-medium">{option}</label>
+                </div>
               ))}
             </div>
-          </>
+          </div>
         )}
       </div>
     </Layout>
