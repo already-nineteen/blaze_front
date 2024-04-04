3432300e828208134993de3dbe794cd9b80c0e1a
jihye1116 <jihyesim170@gmail.com>
2024-04-03 17:06:35 -0700

    chore: 일단 대강 스타일링

--- src/page/Quiz.jsx
+++ src/page/Quiz.jsx
@@ -23,24 +23,30 @@ const Quiz = () => {
 
   return (
     <Layout>
-      <div className="box-border pt-20 px-4 min-h-screen">
+      <div className="box-border pt-20 px-4 min-h-screen flex flex-col items-center justify-center">
         {showScore ? (
-          <div className="score-section">
-            당신의 점수는 {score}점 입니다! 총 {quizData.length}문제 중.
+          <div className="score-section text-lg font-semibold">
+            당신의 점수는 <span className="text-green-500">{score}</span>점
+            입니다! 총 {quizData.length}문제 중.
           </div>
         ) : (
           <>
-            <div className="question-section">
-              <div className="question-count">
-                <span>문제 {currentQuestion + 1}</span>/{quizData.length}
+            <div className="question-section mb-4">
+              <div className="question-count mb-2">
+                <span className="text-xl font-bold">
+                  문제 {currentQuestion + 1}
+                </span>
+                /{quizData.length}
               </div>
-              <div className="question-text">
+              <div className="question-text text-lg font-medium">
                 {quizData[currentQuestion].question}
               </div>
             </div>
-            <div className="answer-section">
-              {quizData[currentQuestion].options.map((option) => (
+            <div className="answer-section grid grid-cols-2 gap-4">
+              {quizData[currentQuestion].options.map((option, index) => (
                 <button
+                  key={index}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                   onClick={() =>
                     handleAnswerOptionClick(
                       option === quizData[currentQuestion].answer
