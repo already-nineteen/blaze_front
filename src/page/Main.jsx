9f31184d4f916edb5aea01d533e0c4e16d753d7c
jihye1116 <jihyesim170@gmail.com>
2024-04-04 17:24:05 -0700

    add: 이미지 파일

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -3,21 +3,13 @@ import Layout from "../components/Layout/Layout";
 const Main = () => {
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center h-full bg-gray-100">
-        <div className="mb-8 text-center">
-          <h1 className="text-2xl font-bold">환영합니다!</h1>
-          <p>
-            FILL은 소방관들의 안전을 위한 서비스입니다. 심박 센서, 가스 센서,
-            충격 감지 센서, 온도 센서가 포함된 소방관의 무선 생명줄입니다.
-            unity에서 블루투스 통신을 통해 센서 정보들이 전달됩니다.
-          </p>
-        </div>
-        <div className="space-x-4">
+      <div className="h-full bg-gray-100">
+        <div className="">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            <a href="/chat">화재 정보에 대해 묻기</a>
+            <a href="/chat">챗봇</a>
           </button>
           <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            <a href="/quiz">소방 관련 퀴즈 풀어보기</a>
+            <a href="/quiz">퀴즈</a>
           </button>
         </div>
       </div>
