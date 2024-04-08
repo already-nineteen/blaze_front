728d70a85c99f88c837cf228ac144a6b8d288f28
jihye1116 <jihyesim170@gmail.com>
2024-04-08 15:47:36 -0700

    feat: 정답 보기 추가

--- src/page/Quiz.jsx
+++ src/page/Quiz.jsx
@@ -3,32 +3,54 @@ import Layout from "../components/Layout/Layout";
 import quizData from "../mocks/quizData.json";
 import CopyToClipboard from "react-copy-to-clipboard";
 
-const link = "http://localhost:3000/quiz";
+const link = "히히 구라지롱";
 
 const Quiz = () => {
   const [quizStarted, setQuizStarted] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
-  // 체크박스 상태를 관리하기 위한 상태 추가
-  const [checkedState, setCheckedState] = useState(false);
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [checkedState, setCheckedState] = useState(
+    new Array(quizData[currentQuestion].options.length).fill(false)
+  );
 
-  const handleAnswerOptionClick = (isCorrect) => {
-    if (isCorrect) {
-      setScore(score + 1);
+  const handleAnswerOptionClick = () => {
+    const correctAnswer = quizData[currentQuestion].answer;
+    const selectedOptionIndex = checkedState.findIndex(
+      (state) => state === true
+    );
+    if (selectedOptionIndex !== -1) {
+      const isCorrect =
+        quizData[currentQuestion].options[selectedOptionIndex] ===
+        correctAnswer;
+      if (isCorrect) {
+        setScore(score + 1);
+      }
     }
 
-    // 체크박스 상태 초기화
-    setCheckedState(false);
-
+    setCheckedState(
+      new Array(quizData[currentQuestion].options.length).fill(false)
+    );
+    setShowAnswer(false);
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < quizData.length) {
       setCurrentQuestion(nextQuestion);
+      setCheckedState(
+        new Array(quizData[nextQuestion].options.length).fill(false)
+      );
     } else {
       setShowScore(true);
     }
   };
 
+  const handleCheckboxChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : false
+    );
+
+    setCheckedState(updatedCheckedState);
+  };
   return (
     <Layout>
       <div className="h-full flex flex-col items-center text-gray-800">
@@ -97,7 +119,7 @@ const Quiz = () => {
             </div>
             <div className="answer-section">
               {quizData[currentQuestion].options.map((option, index) => (
-                <div key={index} className="flex items-center">
+                <div key={index} className="flex items-center my-2">
                   <input
                     type="checkbox"
                     className="appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-500 checked:border-transparent focus:outline-none"
@@ -111,6 +133,32 @@ const Quiz = () => {
                   <label className="ml-2 text-md font-medium">{option}</label>
                 </div>
               ))}
+            </div>
+            {showAnswer && (
+              <div className="text-green-500 font-bold">
+                정답: {quizData[currentQuestion].answer}
+              </div>
+            )}
+            <div className="flex justify-between">
+              <button
+                disabled={currentQuestion === 0}
+                onClick={() => setCurrentQuestion(currentQuestion - 1)}
+                className="bg-blue-500 text-white font-bold py-2 px-4 rounded  hover:bg-blue-700 disabled:opacity-50"
+              >
+                이전
+              </button>
+              <button
+                onClick={() => setShowAnswer(true)}
+                className="bg-yellow-500 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600"
+              >
+                정답 보기
+              </button>
+              <button
+                onClick={() => handleAnswerOptionClick()}
+                className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700"
+              >
+                다음
+              </button>
             </div>
           </div>
         )}
