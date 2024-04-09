31d9b0bca14ee4eede2a8bd2411eb3ae1304aa35
jihye1116 <jihyesim170@gmail.com>
2024-04-09 08:40:27 -0700

    design: 퀴즈 결과 페이지 퍼블리싱쓰

--- src/components/Quiz/ScoreSection.jsx
+++ src/components/Quiz/ScoreSection.jsx
@@ -1,27 +1,29 @@
-import CopyToClipboard from "react-copy-to-clipboard";
-
-const link = "히히 구라지롱";
+import { Link } from "react-router-dom";
 
 const ScoreSection = ({ score, totalQuestions }) => {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <div className="score-section text-lg font-semibold">
-        점수는요..
-        <p className="text-7xl">
-          <span className="text-green-500">
-            {score}/{totalQuestions}
-          </span>
-          점
+      <div className="score-section text-lg font-semibold space-y-16">
+        <div className="flex flex-col space-y-4">
+          <p>퀴즈 결과, 맞춘 개수는...</p>
+          <p className="text-5xl text-green-500" style={{ color: "#FFE606" }}>
+            {score}
+            <span className="text-4xl text-gray-300">/{totalQuestions}</span>
+          </p>
+        </div>
+        <p className="text-xl">
+          대단해요!
+          <br />
+          소방 안전 지식을 잘 알고 있는 것 같아요.
         </p>
+        <div className="flex justify-end w-96">
+          <Link to={"/quiz"}>
+            <button className="text-base mt-4 text-gray-300 bg-gray-100 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-lg">
+              처음으로 돌아가기
+            </button>
+          </Link>
+        </div>
       </div>
-      <CopyToClipboard
-        text={link}
-        onCopy={() => alert("링크가 클립보드에 복사되었습니다!")}
-      >
-        <button className="mt-4 bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-          공유하기
-        </button>
-      </CopyToClipboard>
     </div>
   );
 };
