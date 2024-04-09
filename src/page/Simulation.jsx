8c1860ba63a3510010f1fc447a447c8d75e2e94c
jihye1116 <jihyesim170@gmail.com>
2024-04-09 10:53:21 -0700

    design: 시뮬레이션 이쁘게 퍼블리싱쓰~~

--- src/page/Simulation.jsx
+++ src/page/Simulation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import Layout from "../components/Layout/Layout";
 
 function SimulationPage() {
   const { unityProvider } = useUnityContext({
@@ -9,7 +10,17 @@ function SimulationPage() {
     codeUrl: "Build/fill_unity.wasm",
   });
 
-  return <Unity unityProvider={unityProvider} />;
+  return (
+    <Layout>
+      <div className="flex items-center justify-center h-full">
+        <Unity
+          className="border-l-8 border-r-8 rounded-lg"
+          style={{ width: "80%", height: "80%", borderColor: "#FFF6A3" }}
+          unityProvider={unityProvider}
+        />
+      </div>
+    </Layout>
+  );
 }
 
 export default SimulationPage;
