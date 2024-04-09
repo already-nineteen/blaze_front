899bee44b57abe3ff54e1eaaff881b40f6e654bc
jihye1116 <jihyesim170@gmail.com>
2024-04-09 11:26:40 -0700

    add: 시뮬레이션 인트로 추가쓰.. 근데 아직 qr 남았다

--- src/page/Simulation.jsx
+++ src/page/Simulation.jsx
@@ -1,6 +1,7 @@
-import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import Layout from "../components/Layout/Layout";
+import SimulationIntro from "../components/Simulation/SimulationIntro";
+import { useState } from "react";
 
 function SimulationPage() {
   const { unityProvider } = useUnityContext({
@@ -10,14 +11,24 @@ function SimulationPage() {
     codeUrl: "Build/fill_unity.wasm",
   });
 
+  const [showSimulation, setShowSimulation] = useState(false);
+
+  const handleStartSimulation = () => {
+    setShowSimulation(true);
+  };
+
   return (
     <Layout>
       <div className="flex items-center justify-center h-full">
-        <Unity
-          className="border-l-8 border-r-8 rounded-lg"
-          style={{ width: "80%", height: "80%", borderColor: "#FFF6A3" }}
-          unityProvider={unityProvider}
-        />
+        {!showSimulation ? (
+          <SimulationIntro onStart={handleStartSimulation} />
+        ) : (
+          <Unity
+            className="border-l-8 border-r-8 rounded-lg"
+            style={{ width: "80%", height: "80%", borderColor: "#FFF6A3" }}
+            unityProvider={unityProvider}
+          />
+        )}
       </div>
     </Layout>
   );
