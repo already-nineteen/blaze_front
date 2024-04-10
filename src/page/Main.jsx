ab532c0c742d8e2f8f89bb5c9fdc1b7f6196355c
원설아 <seolalny@gmail.com>
2024-04-10 22:34:19 +0900

    add: 메인 페이지 퍼블리싱

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,28 +1,58 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import styled from "styled-components";
+import mainIntroImg from "../assets/img/main-intro.png";
+
 const Main = () => {
   return (
     <Layout>
+      <MainContainer>
       <div className="flex flex-col items-center justify-center h-full">
-        <div className="mb-8 text-center">
-          <h1 className="text-2xl font-bold">환영합니다!</h1>
-          <p>
-            FILL은 소방관들의 안전을 위한 서비스입니다. 심박 센서, 가스 센서,
-            충격 감지 센서, 온도 센서가 포함된 소방관의 무선 생명줄입니다.
-            unity에서 블루투스 통신을 통해 센서 정보들이 전달됩니다.
-          </p>
-        </div>
-        <div className="space-x-4">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            <a href="/chat">화재 정보에 대해 묻기</a>
-          </button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            <a href="/quiz">소방 관련 퀴즈 풀어보기</a>
-          </button>
-        </div>
+        <Row>
+          <div>
+            <Img src={mainIntroImg} />
+          </div>
+          <div>
+            <div className="mb-8 text-center">
+              <P><HighLight>화재</HighLight>, 일상 중 언제든 일어날 수 있습니다.</P>
+            </div>
+            <div className="space-x-4">
+              <P>이름과 함께 화재에 대한 지식을 습득하고 <br />
+              대처 상황을 경험해 보며 훈련해 봐요.</P>
+            </div>
+          </div>
+        </Row>
       </div>
+      </MainContainer>
     </Layout>
   );
 };
 
 export default Main;
+
+const Img = styled.img`
+  
+`;
+
+const HighLight = styled.span`
+  background-color: #FFF6A3;
+  font-family: 'PretendardSemiBold';
+  font-size: 26px;
+`;
+
+const P = styled.p`
+  font-family: 'PretendardSemiBold';
+  font-size: 26px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 80px;
+`;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,254,247,1) 100%);
+`;
