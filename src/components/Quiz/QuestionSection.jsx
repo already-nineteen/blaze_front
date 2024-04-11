6a8b1441bad0cf38a46cf9ec04eeeb27f5ac9581
jihye1116 <jihyesim170@gmail.com>
2024-04-10 23:24:40 -0700

    design: 퀴즈 섹션 퍼블리싱 대강 한듯

--- src/components/Quiz/QuestionSection.jsx
+++ src/components/Quiz/QuestionSection.jsx
@@ -11,51 +11,62 @@ const QuestionSection = ({
   handleAnswerOptionClick,
 }) => {
   return (
-    <div className="question-section pt-40 px-4 mb-8 w-96 flex flex-col gap-8">
-      <div className="question-count mb-2">
-        <span className="text-xl font-bold">문항 {currentQuestion + 1}</span>/
-        {quizData.length}
+    <div className="question-section flex flex-col justify-center h-full gap-8 w-2/5">
+      <div className="question-count mb-2 text-md text-gray-300 w-full flex justify-center">
+        {currentQuestion + 1} / {quizData.length}
       </div>
-      <div className="question-text text-lg font-medium">
-        {quizData[currentQuestion].question}
+      <div className="question-text text-lg font-medium flex gap-2 px-4">
+        <div
+          className="text-xl text-white rounded-full min-w-10 min-h-10 max-h-10 flex items-center justify-center"
+          style={{
+            backgroundColor: "#FFE606",
+          }}
+        >
+          Q.
+        </div>
+        <div className="text-xl mt-1">{quizData[currentQuestion].question}</div>
       </div>
-      <div className="answer-section">
+      <div className="answer-section w-full box-border px-16 mb-10">
         {quizData[currentQuestion].options.map((option, index) => (
           <div key={index} className="flex items-center my-2">
             <input
               type="checkbox"
-              className="appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-500 checked:border-transparent focus:outline-none"
+              className="form-checkbox appearance-none h-6 w-6 border bg-gray-300 rounded-full"
               checked={checkedState[index]}
               onChange={() => handleCheckboxChange(index)}
             />
-            <label className="ml-2 text-md font-medium">{option}</label>
+            <label className="ml-2 text-md font-medium text-gray-300">
+              {option}
+            </label>
           </div>
         ))}
+        {showAnswer && (
+          <div className="w-full mt-6">
+            정답: {quizData[currentQuestion].answer}
+          </div>
+        )}
       </div>
-      {showAnswer && (
-        <div className="text-green-500 font-bold">
-          정답: {quizData[currentQuestion].answer}
-        </div>
-      )}
-      <div className="flex justify-between">
+      <div className="w-full flex justify-between items-center text-gray-300 ">
         <button
           disabled={currentQuestion === 0}
           onClick={() => setCurrentQuestion(currentQuestion - 1)}
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 disabled:opacity-50"
+          className="flex gap-1 py-2 px-4 rounded hover:bg-gray-100"
         >
+          <img src="assets/<-.svg" />
           이전
         </button>
         <button
           onClick={() => setShowAnswer(true)}
-          className="bg-yellow-500 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600"
+          className="py-2 px-4 rounded hover:bg-gray-100"
         >
           정답 보기
         </button>
         <button
           onClick={() => handleAnswerOptionClick(currentQuestion)}
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700"
+          className="flex gap-1 py-2 px-4 rounded hover:bg-gray-100"
         >
           다음
+          <img className="rotate-180" src="assets/<-.svg" />
         </button>
       </div>
     </div>
